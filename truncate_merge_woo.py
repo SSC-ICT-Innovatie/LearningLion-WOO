ff1b6e3e8ef2ae4bfe7487b5d610ac4176c35564
JuNicky <Y.H.Ju@student.tudelft.nl>
2024-04-16 09:44:10 +0200

    remove loguru, add support for reading multiple bodytexts

--- truncate_merge_woo.py
+++ truncate_merge_woo.py
@@ -1,14 +1,14 @@
+import glob
 import time
 import os
 import pandas as pd
-from loguru import logger
 
 def select_woogle_dump_folders(path:str='./docs') -> str:
     """
     Look for folders containing "WoogleDumps" in their names and return the selected folder.
     """
     if not os.path.exists(path) or not os.path.isdir(path):
-        logger.error(f'Path: "{path}" does not exist or is not a directory.')
+        print(f'Path: "{path}" does not exist or is not a directory.')
         return None
     
     # Look for folders containing "woogle_dump" in their names
@@ -19,7 +19,7 @@ def select_woogle_dump_folders(path:str='./docs') -> str:
         print(f"{idx}. {folder}")
 
     if not folders_with_woogle_dumps:
-        logger.error("No folders containing 'WoogleDumps' were found.")
+        print("No folders containing 'WoogleDumps' were found.")
         return None
     
     selection = int(input("Select a folder by number: ")) - 1
@@ -32,20 +32,27 @@ def read_docs(path:str) -> tuple[pd.DataFrame, pd.DataFrame, pd.DataFrame]:
     # Manually set the correct types
     bodytext_dtypes = {'dc_publisher_name': str, 'dc_publisher': str, 'foi_documentId': str, 'foi_pageNumber': int, 'foi_bodyText': str, 'foi_bodyTextOCR': str, 'foi_hasOCR': bool, 'foi_redacted': float, 'foi_contourArea': float, 'foi_textArea': float, 'foi_charArea': float, 'foi_percentageTextAreaRedacted': float, 'foi_percentageCharAreaRedacted': float, 'foi_nrWords': int, 'foi_nrChars': int, 'foi_nrWordsOCR ': int, 'foi_nrCharsOCR': int}
     dossier_dtypes = {'dc_identifier': str, 'dc_title': str, 'dc_description': str, 'dc_type': str, 'foi_type_description': str, 'dc_publisher_name': str, 'dc_publisher': str, 'dc_source': str, 'foi_valuation': str, 'foi_requestText': str, 'foi_decisionText': str, 'foi_isAdjourned': str, 'foi_requester': str}
-    document_dtypes = {'dc_identifier': str, 'foi_dossierId': str, 'dc_title': str, 'foi_fileName': str, 'dc_format': str, 'dc_source': str, 'dc_type': str, 'foi_nrPages': int}
+    document_dtypes = {'dc_identifier': str, 'foi_dossierId': str, 'dc_title': str, 'foi_fileName': str, 'dc_format': str, 'dc_source': str, 'dc_type': str, 'foi_nrPages': float}
     
     # Start recording the time
     start_time = time.time()
-    logger.info(f"Reading data from {path}...")
+    print(f"Reading data from {path}...")
     
-    bodytext_dataframe = pd.read_csv(f'{path}/woo_bodytext.csv.gz', dtype=bodytext_dtypes).set_index('foi_documentId')
+    # Get all the bodytext dataframes, because they are split over multiple files
+    files = glob.glob(f'{path}/woo_bodytext*.csv.gz')
+    dataframes = []
+    for file in files:
+        df = pd.read_csv(file, dtype=bodytext_dtypes).set_index('foi_documentId')
+        dataframes.append(df)
+    bodytext_dataframe = pd.concat(dataframes)
+
     # Manually changes some columns, has to be done seperately due to nan values
     bodytext_dataframe['foi_redacted'] = bodytext_dataframe['foi_redacted'].astype(bool)
     dossier_dataframe = pd.read_csv(f'{path}/woo_dossiers.csv.gz', parse_dates=['foi_publishedDate', 'dc_date_year', 'foi_requestDate', 'foi_decisionDate', 'foi_retrievedDate'], dtype=dossier_dtypes).set_index('dc_identifier')
     document_dataframe = pd.read_csv(f'{path}/woo_documents.csv.gz', dtype=document_dtypes).set_index('dc_identifier')
     
     # Log time it took
-    logger.info(f"Data read in {time.time() - start_time:.2f} seconds.")
+    print(f"Data read in {time.time() - start_time:.2f} seconds.")
     
     return bodytext_dataframe, dossier_dataframe, document_dataframe
 
@@ -56,23 +63,25 @@ def save_docs(df:pd.DataFrame, filename:str) -> None:
     if not os.path.exists(filename):
         df.to_csv(filename, compression='gzip')
     else:
-        logger.warning(f"Skipping file as it already exists at location: {filename}")
+        print(f"Skipping file as it already exists at location: {filename}")
 
-def truncate_merge_docs(length:int=10) -> None:
+def truncate_merge_docs(length:int=-1) -> None:
     """
     Truncates the bodytext, dossier and document dataframes to the given length and saves them in a new directory.
     Also merges the dataframes and saves the result in the new directory.
     """
     path = select_woogle_dump_folders()
     if path is None:
-        logger.error("No folder was selected. Exiting...")
+        print("No folder was selected. Exiting...")
         return
 
     bodytext_dataframe, dossier_dataframe, document_dataframe = read_docs(path)
     
     nr_dossiers, _ = dossier_dataframe.shape
-    if length > nr_dossiers:
-        logger.error(f"Length is larger than the number of dossiers in the dataset. Exiting...")
+    if length == -1:
+      length = nr_dossiers  
+    elif length > nr_dossiers:
+        print(f"Length is larger than the number of dossiers in the dataset. Exiting...")
         return None
 
     # Define the new directory based on path and length
@@ -117,8 +126,8 @@ def truncate_merge_docs(length:int=10) -> None:
     result = result[result['dossiers_dc_type_description'] == 'Dossier na Woo/Wob-verzoek']
     
     save_docs(result, f'{new_directory}/woo_merged.csv.gz')
-    
-    logger.info(f"Truncated data successfully saved to {new_directory}.")
+       
+    print(f"Truncated data successfully saved to {new_directory}.")
 
 if __name__ == "__main__":
     truncate_merge_docs()
