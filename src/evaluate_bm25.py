4b84123e32b651c9b0797513e511dc64dcf3dc08
JuNicky <Y.H.Ju@student.tudelft.nl>
2024-06-24 14:36:26 +0200

    refactor bm25, to use timer and csvwriter consistently

--- src/evaluate_bm25.py
+++ src/evaluate_bm25.py
@@ -1,89 +1,33 @@
-# Example with arguments:
-# python evaluate_bm25.py --algorithm BM25Okapi --content_folder_name WoogleDumps_01-04-2024_10_dossiers_no_requests_fake_stopwords --documents_directory ../docs --evaluation_directory ./evaluation --evaluation_file evaluation_request_WoogleDumps_01-04-2024_10_dossiers_no_requests.json --results_path ./evaluation/results
-# python evaluate_bm25.py --algorithm BM25Okapi --content_folder_name 12_dossiers_no_requests --documents_directory ./docs --evaluation_directory ./evaluation --evaluation_file evaluation_request_12_dossiers_no_requests.json --results_path ./evaluation/results
-# python evaluate_bm25.py --algorithm BM25Okapi --content_folder_name 12_dossiers_no_requests --documents_directory ./docs --evaluation_directory ./evaluation --evaluation_file evaluation_request_60_dossiers_no_requests.json --results_path ./evaluation/results
+"""
+Example with arguments:
+python evaluate_bm25.py --algorithm BM25Okapi --content_folder_name WoogleDumps_01-04-2024_10_dossiers_no_requests_fake_stopwords --documents_directory ../docs --evaluation_directory ./evaluation --evaluation_file evaluation_request_WoogleDumps_01-04-2024_10_dossiers_no_requests.json --results_path ./evaluation/results
+python evaluate_bm25.py --algorithm BM25Okapi --content_folder_name 12_dossiers_no_requests --documents_directory ./docs --evaluation_directory ./evaluation --evaluation_file evaluation_request_12_dossiers_no_requests.json --results_path ./evaluation/results
+python evaluate_bm25.py --algorithm BM25Okapi --content_folder_name 12_dossiers_no_requests --documents_directory ./docs --evaluation_directory ./evaluation --evaluation_file evaluation_request_60_dossiers_no_requests.json --results_path ./evaluation/results
+"""
 
-import csv
 import heapq
 import json
 import nltk
-import os
 import pandas as pd
 from argparse import ArgumentParser
 from common import evaluate_helpers
+from common.csv_writer import CSVWriter
 from common.fastbm25 import FastBM25
+from common.register_time import Timer
+from itertools import islice
 from rank_bm25 import BM25Okapi, BM25L, BM25Plus
 
 
-def run_bm25(woo_data, bm25, evaluation, evaluation_file, content_folder_name, results_path):
-    # Check if chunks are present in the data
-    print(f"[Info] ~ Running algorithm: {bm25.__class__.__name__}", flush=True)
+def run_bm25(woo_data, bm25, evaluation, evaluation_file, content_folder_name, results_path, timer):
+    # Initialize CSV Writer Object
+    csv_writer = CSVWriter(content_folder_name, bm25.__class__.__name__, evaluation_file=evaluation_file, folder_name=results_path)
 
-    # Determine file paths and check existence
-    csv_file_path = f'{results_path}/{evaluation_file.split(".")[0]}_{content_folder_name}_{bm25.__class__.__name__}_request.csv'
-    last_index = -1
+    # Find starting index
+    start_index = csv_writer.last_index + 1
+    if start_index > 0:
+        print(f"[Info] ~ Skipping until index {start_index - 1}.", flush=True)
 
-    # If the file exists, determine the last index processed
-    if os.path.exists(csv_file_path):
-        with open(csv_file_path, "r") as file:
-            reader = csv.reader(file)
-            last_index = sum(1 for row in reader)
-
-    # Open CSV file for appending or writing
-    csv_file = open(csv_file_path, "a", newline="")
-    csv_writer = csv.writer(csv_file)
-
-    # Write header if the file is new
-    if last_index == -1:
-        csv_writer.writerow(
-            [
-                "page_id",
-                "dossier_id",
-                "retrieved_page_ids",
-                "retrieved_dossier_ids",
-                "scores",
-                "number_of_correct_dossiers",
-                *(f"dossier#{i+1}" for i in range(20)),
-            ]
-        )
-
-    result = pd.DataFrame(
-        columns=[
-            "page_id",
-            "dossier_id",
-            "retrieved_page_ids",
-            "retrieved_dossier_ids",
-            "scores",
-            "number_of_correct_dossiers",
-            "dossier#1",
-            "dossier#2",
-            "dossier#3",
-            "dossier#4",
-            "dossier#5",
-            "dossier#6",
-            "dossier#7",
-            "dossier#8",
-            "dossier#9",
-            "dossier#10",
-            "dossier#11",
-            "dossier#12",
-            "dossier#13",
-            "dossier#14",
-            "dossier#15",
-            "dossier#16",
-            "dossier#17",
-            "dossier#18",
-            "dossier#19",
-            "dossier#20",
-        ]
-    )
-
-    print("[Info] ~ Starting with the first item", flush=True)
-
-    for index, (key, value) in enumerate(evaluation.items()):
-        if index <= last_index:
-            print(f"[Info] ~ Skipping index {index}", flush=True)
-            continue
+    for index, (key, value) in enumerate(islice(evaluation.items(), start_index, None)):
         if not value.get("pages"):
             print("[Warning] ~ No pages found in the JSON file", flush=True)
             continue
@@ -96,64 +40,36 @@ def run_bm25(woo_data, bm25, evaluation, evaluation_file, content_folder_name, r
 
         tokenized_query = evaluate_helpers.preprocess_text(key)
         # tokenized_query = evaluate_helpers.tokenize(key)
-        retrieved_page_ids = []
-        retrieved_dossier_ids = []
-        # Check if running fastbm25 or not, because FastBM25 is from a different package
+        
+        # Check if running FastBM25 or not, because FastBM25 uses different code
         if bm25.__class__.__name__ == "FastBM25":
-            n_pages_result = bm25.top_k_sentence_index(tokenized_query, k=20)
+            doc_scores = bm25.top_scores(tokenized_query)
+            n_pages_result = heapq.nlargest(21, doc_scores, key=doc_scores.__getitem__)
         else:
             doc_scores = bm25.get_scores(tokenized_query)
-            n_pages_result = heapq.nlargest(20, range(len(doc_scores)), key=doc_scores.__getitem__)
+            n_pages_result = heapq.nlargest(21, range(len(doc_scores)), key=doc_scores.__getitem__)
+            
+        retrieved_page_ids = []
+        retrieved_dossier_ids = []
+        scores = []
 
         for i in n_pages_result:
             retrieved_page_ids.append(woo_data["page_id"][i])
             retrieved_dossier_ids.append(woo_data["dossier_id"][i])
+            scores.append(doc_scores[i])
 
-        # Collect top documents and their scores for the current BM25 algorithm
-        new_row = {
-            "page_id": "N/A",
-            "dossier_id": value["dossier"][0],
-            "retrieved_page_ids": ", ".join(retrieved_page_ids),
-            "retrieved_dossier_ids": ", ".join(retrieved_dossier_ids),
-            "scores": "",
-            "number_of_correct_dossiers": retrieved_dossier_ids.count(value["dossier"][0]),
-            "dossier#1": retrieved_dossier_ids[0] == value["dossier"][0],
-            "dossier#2": retrieved_dossier_ids[1] == value["dossier"][0],
-            "dossier#3": retrieved_dossier_ids[2] == value["dossier"][0],
-            "dossier#4": retrieved_dossier_ids[3] == value["dossier"][0],
-            "dossier#5": retrieved_dossier_ids[4] == value["dossier"][0],
-            "dossier#6": retrieved_dossier_ids[5] == value["dossier"][0],
-            "dossier#7": retrieved_dossier_ids[6] == value["dossier"][0],
-            "dossier#8": retrieved_dossier_ids[7] == value["dossier"][0],
-            "dossier#9": retrieved_dossier_ids[8] == value["dossier"][0],
-            "dossier#10": retrieved_dossier_ids[9] == value["dossier"][0],
-            "dossier#11": retrieved_dossier_ids[10] == value["dossier"][0],
-            "dossier#12": retrieved_dossier_ids[11] == value["dossier"][0],
-            "dossier#13": retrieved_dossier_ids[12] == value["dossier"][0],
-            "dossier#14": retrieved_dossier_ids[13] == value["dossier"][0],
-            "dossier#15": retrieved_dossier_ids[14] == value["dossier"][0],
-            "dossier#16": retrieved_dossier_ids[15] == value["dossier"][0],
-            "dossier#17": retrieved_dossier_ids[16] == value["dossier"][0],
-            "dossier#18": retrieved_dossier_ids[17] == value["dossier"][0],
-            "dossier#19": retrieved_dossier_ids[18] == value["dossier"][0],
-            "dossier#20": retrieved_dossier_ids[19] == value["dossier"][0],
-        }
-
-        # Append the new value to the DataFrame
-        # result.append(new_row, ignore_index=True)
-        result.loc[len(result)] = new_row
-
-        csv_writer.writerow(
+        csv_writer.write_row(
             [
                 "N/A",
                 value["dossier"][0],
                 ", ".join(retrieved_page_ids),
                 ", ".join(retrieved_dossier_ids),
-                "",
+                ", ".join(map(str, scores)),
                 retrieved_dossier_ids.count(value["dossier"][0]),
-                *(retrieved_dossier_ids[i] == value["dossier"][0] for i in range(20)),
+                *((retrieved_dossier_ids[i] == value["dossier"][0] if i < len(retrieved_dossier_ids) else False) for i in range(20)),
             ]
         )
+        timer.update_time()
         print(f"[Info] ~ Results written on index: {index}.", flush=True)
 
 
@@ -162,39 +78,23 @@ def main():
     nltk.download("punkt", quiet=True)
     nltk.download("stopwords", quiet=True)
 
-    print("[Info] ~ Successfully downloaded the NLTK resources.", flush=True)
-
     parser = ArgumentParser()
-    parser.add_argument("--algorithm", type=str, required=True)
+    parser.add_argument("--algorithm", type=str, choices=["BM25Okapi", "BM25L", "BM25Plus", "BM25Fast"], required=True)
     parser.add_argument("--content_folder_name", type=str, required=True)
     parser.add_argument("--documents_directory", type=str, required=True)
     parser.add_argument("--evaluation_directory", type=str, required=True)
     parser.add_argument("--evaluation_file", type=str, required=True)
     parser.add_argument("--retrieve_whole_document", type=bool, default=False)
     parser.add_argument("--results_path", type=str, required=True)
-
     args = parser.parse_args()
 
-    content_folder_name = args.content_folder_name
-    documents_directory = args.documents_directory
-    evaluation_directory = args.evaluation_directory
-    evaluation_file = args.evaluation_file
-    retrieve_whole_document = args.retrieve_whole_document
-    results_path = args.results_path
-    if args.algorithm in ["BM25Okapi", "BM25L", "BM25Plus", "BM25Fast"]:
-        algorithm = args.algorithm
-    else:
-        raise ValueError("Algorithm must be one of: BM25Okapi, BM25L, BM25Plus, BM25Fast.")
-
-    print(f"[Info] ~ Source folder of documents: {content_folder_name}", flush=True)
-
     # Selecting the paths
-    input_path = f"{documents_directory}/{content_folder_name}/woo_merged.csv.gz"
-    evaluation_path = f"{evaluation_directory}/{evaluation_file}"
+    input_path = f"{args.documents_directory}/{args.content_folder_name}/woo_merged.csv.gz"
+    evaluation_path = f"{args.evaluation_directory}/{args.evaluation_file}"
     woo_data = pd.read_csv(input_path, compression="gzip")
 
     # Preprocess woo data, merge all the pages into one document, instead of seperately
-    if retrieve_whole_document:
+    if args.retrieve_whole_document:
         woo_data = (
             woo_data.groupby("document_id")
             .agg(
@@ -206,35 +106,37 @@ def main():
             )
             .reset_index()
         )
+        
+    # Initializing Timer
+    timer = Timer(args.content_folder_name, args.algorithm, evaluation_file=args.evaluation_file)
 
     # Generate corpus, which is a list of all the words per document
     corpus = woo_data["bodyText"].tolist()
-
     print(f"[Info] ~ Number of documents in corpus: {len(corpus)}", flush=True)
 
     # Do preprocessing for each document
     tokenized_corpus = [evaluate_helpers.preprocess_text(doc) for doc in corpus]
 
+    # Read evaluation File
     with open(evaluation_path, "r") as file:
         evaluation = json.load(file)
-
     print(f"[Info] ~ Number of documents in evaluation: {len(evaluation)}", flush=True)
 
-    if algorithm == "BM25Okapi":
+    # Choose the appropriate algorithm
+    if args.algorithm == "BM25Okapi":
         bm25 = BM25Okapi(tokenized_corpus)
-    elif algorithm == "BM25L":
+    elif args.algorithm == "BM25L":
         bm25 = BM25L(tokenized_corpus)
-    elif algorithm == "BM25Plus":
+    elif args.algorithm == "BM25Plus":
         bm25 = BM25Plus(tokenized_corpus)
-    elif algorithm == "BM25Fast":
+    elif args.algorithm == "BM25Fast":
         bm25 = FastBM25(tokenized_corpus)
     else:
         raise ValueError("Algorithm must be one of: BM25Okapi, BM25L, BM25Plus, BM25Fast.")
 
-    print(f"[Info] ~ Starting {algorithm}", flush=True)
-    run_bm25(woo_data, bm25, evaluation, evaluation_file, content_folder_name, results_path)
-    print(f"[Info] ~ {algorithm} done", flush=True)
-
+    print(f"[Info] ~ Starting {args.algorithm}", flush=True)
+    run_bm25(woo_data, bm25, evaluation, args.evaluation_file, args.content_folder_name, args.results_path, timer)
+    print(f"[Info] ~ {args.algorithm} done", flush=True)
 
 if __name__ == "__main__":
     main()
--- src/evaluate_bm25_document_similarity.py
+++ src/evaluate_bm25_document_similarity.py
@@ -3,131 +3,109 @@ Examples with arguments:
 python evaluate_bm25_document_similarity.py --algorithm BM25Fast --content_folder_name minaz_no_requests --documents_directory /scratch/nju/docs_ministries_full --results_path /scratch/nju/evaluation_ministries_full/results
 """
 
-import csv
 import heapq
-import os
 import pandas as pd
 from argparse import ArgumentParser
-from rank_bm25 import BM25Okapi, BM25L, BM25Plus
 from common import evaluate_helpers
+from common.csv_writer import CSVWriter
 from common.fastbm25 import FastBM25
+from common.register_time import Timer
+from rank_bm25 import BM25Okapi, BM25L, BM25Plus
 
 
-def run_bm25_document_similarity(woo_data, bm25, results_path, content_folder_name):
-    print(f"[Info] ~ Running algorithm: {bm25.__class__.__name__}", flush=True)
+def run_bm25_document_similarity(woo_data, bm25, results_path, content_folder_name, timer):
+    # Initialize CSV Writer Object
+    csv_writer = CSVWriter(content_folder_name, bm25.__class__.__name__, document_similarity=True, folder_name=results_path)
 
-    # Determine file paths and check existence
-    csv_file_path = f"{results_path}/document_similarity_{content_folder_name}_{bm25.__class__.__name__}.csv"
-    last_index = -1
+    # Find starting index
+    start_index = csv_writer.last_index + 1
+    if start_index > 0:
+        print(f"[Info] ~ Skipping until index {start_index - 1}.", flush=True)
 
-    # If the file exists, determine the last index processed
-    if os.path.exists(csv_file_path):
-        with open(csv_file_path, "r") as file:
-            reader = csv.reader(file)
-            last_index = sum(1 for row in reader)
-
-    # Open CSV file for appending or writing
-    csv_file = open(csv_file_path, "a", newline="")
-    csv_writer = csv.writer(csv_file)
-
-    # Write header if the file is new
-    if last_index == -1:
-        csv_writer.writerow(
-            [
-                "page_id",
-                "dossier_id",
-                "retrieved_page_ids",
-                "retrieved_dossier_ids",
-                "scores",
-                "number_of_correct_dossiers",
-                *(f"dossier#{i+1}" for i in range(20)),
-            ]
-        )
-
-    for index, (_, row) in enumerate(woo_data.iterrows()):
-        if index <= last_index:
-            print(f"[Info] ~ Skipping index {index}", flush=True)
-            continue
+    for index, row in woo_data.iloc[start_index:].iterrows():
         if pd.isna(row["bodyText"]):
             continue
-        tokenized_query = evaluate_helpers.tokenize(row["bodyText"])
+        
+        tokenized_query = evaluate_helpers.preprocess_text(row["bodyText"])
+        # tokenized_query = evaluate_helpers.tokenize(row["bodyText"])
 
-        # Check if running fastbm25 or not, because FastBM25 is from a different package
+        # Check if running FastBM25 or not, because FastBM25 uses different code
         if bm25.__class__.__name__ == "FastBM25":
-            n_pages_result = bm25.top_k_sentence_index(tokenized_query, k=21)
+            doc_scores = bm25.top_scores(tokenized_query)
+            n_pages_result = heapq.nlargest(21, doc_scores, key=doc_scores.__getitem__)
         else:
             doc_scores = bm25.get_scores(tokenized_query)
             n_pages_result = heapq.nlargest(21, range(len(doc_scores)), key=doc_scores.__getitem__)
-
+            
         retrieved_page_ids = []
         retrieved_dossier_ids = []
-        # scores = []
+        scores = []
 
         for i in n_pages_result:
             if woo_data["page_id"][i] == row["page_id"]:
-                print("[Info] ~ Same document retrieved", flush=True)
+                # print("[Info] ~ Same document retrieved", flush=True)
                 continue
             if len(retrieved_page_ids) == 20:
-                print("[Info] ~ 20 documents retrieved", flush=True)
+                # print("[Info] ~ 20 documents retrieved", flush=True)
                 break
             retrieved_page_ids.append(woo_data["page_id"][i])
             retrieved_dossier_ids.append(woo_data["dossier_id"][i])
+            scores.append(doc_scores[i])
 
-        # Append the new row to the DataFrame
-        csv_writer.writerow(
+        csv_writer.write_row(
             [
                 row["page_id"],
                 row["dossier_id"],
                 ", ".join(retrieved_page_ids),
                 ", ".join(retrieved_dossier_ids),
-                "",
+                ", ".join(map(str, scores)),
                 retrieved_dossier_ids.count(row["dossier_id"]),
-                *((retrieved_dossier_ids[i] == row["dossier_id"] if i < len(retrieved_dossier_ids) else False) for i in range(20)),  # Dossiers 1-20
+                *((retrieved_dossier_ids[i] == row["dossier_id"] if i < len(retrieved_dossier_ids) else False) for i in range(20)),
             ]
         )
+        timer.update_time()
         print(f"[Info] ~ Results written on index: {index}.", flush=True)
+
+    csv_writer.close()
 
 
 def main():
     parser = ArgumentParser()
-    parser.add_argument("--algorithm", type=str, required=True)
+    parser.add_argument("--algorithm", type=str, choices=["BM25Okapi", "BM25L", "BM25Plus", "BM25Fast"], required=True)
     parser.add_argument("--content_folder_name", type=str, required=True)
     parser.add_argument("--documents_directory", type=str, required=True)
     parser.add_argument("--results_path", type=str, required=True)
     args = parser.parse_args()
 
-    content_folder_name = args.content_folder_name
-    documents_directory = args.documents_directory
-    results_path = args.results_path
-    if args.algorithm in ["BM25Okapi", "BM25L", "BM25Plus", "BM25Fast"]:
-        algorithm = args.algorithm
-
     # Selecting the paths
-    input_path = f"{documents_directory}/{content_folder_name}/woo_merged.csv.gz"
+    input_path = f"{args.documents_directory}/{args.content_folder_name}/woo_merged.csv.gz"
     woo_data = pd.read_csv(input_path, compression="gzip")
+    
+    # Initializing Timer
+    timer = Timer(args.content_folder_name, args.algorithm, document_similarity="document_similarity")
 
     # Generate corpus, which is a list of all the words per document
     corpus = woo_data["bodyText"].tolist()
-
     print(f"[Info] ~ Number of documents in corpus: {len(corpus)}", flush=True)
 
-    # Do preprocessing for each document
+    # Do preprocessing and tokenization for each document
     tokenized_corpus = [evaluate_helpers.preprocess_text(doc) for doc in corpus]
 
-    if algorithm == "BM25Okapi":
+    # Choose the appropriate algorithm
+    if args.algorithm == "BM25Okapi":
         bm25 = BM25Okapi(tokenized_corpus)
-    elif algorithm == "BM25L":
+    elif args.algorithm == "BM25L":
         bm25 = BM25L(tokenized_corpus)
-    elif algorithm == "BM25Plus":
+    elif args.algorithm == "BM25Plus":
         bm25 = BM25Plus(tokenized_corpus)
-    elif algorithm == "BM25Fast":
+    elif args.algorithm == "BM25Fast":
         bm25 = FastBM25(tokenized_corpus)
     else:
         raise ValueError("Algorithm must be one of: BM25Okapi, BM25L, BM25Plus, BM25Fast.")
 
-    print(f"[Info] ~ Starting {algorithm}", flush=True)
-    run_bm25_document_similarity(woo_data, bm25, results_path, content_folder_name)
-    print(f"[Info] ~ {algorithm} done", flush=True)
+    print(f"[Info] ~ Starting {args.algorithm}.", flush=True)
+    run_bm25_document_similarity(woo_data, bm25, args.results_path, args.content_folder_name, timer)
+    print(f"[Info] ~ {args.algorithm} done.", flush=True)
 
 
 if __name__ == "__main__":
