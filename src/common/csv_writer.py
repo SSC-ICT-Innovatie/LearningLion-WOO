211976ce029745a771a9188464081f6f3ff219d0
JuNicky <Y.H.Ju@student.tudelft.nl>
2024-06-24 14:16:21 +0200

    add csv_writer, refactor register time

---
+++ src/common/csv_writer.py
@@ -0,0 +1,62 @@
+import csv
+import os
+
+class CSVWriter:
+    def __init__(self, content_folder_name, algorithm, evaluation_file="", document_similarity=False, folder_name="./evaluation/results"):
+
+        self.file_name = self._generate_file_name(content_folder_name, algorithm, evaluation_file, document_similarity)
+        self.file_path = os.path.join(folder_name, self.file_name)
+        if not os.path.exists(folder_name):
+            os.makedirs(folder_name)
+        self.last_index = self._determine_last_index()
+        self.csv_file = None
+        self.csv_writer = None
+        self._open()
+
+    def _generate_file_name(self, content_folder_name, algorithm, evaluation_file, document_similarity):
+        if len(evaluation_file) != 0:
+            evaluation_file = evaluation_file.split('.')[0]
+            return f"evaluation_{content_folder_name}_{evaluation_file}_{algorithm}.csv"
+        if document_similarity:
+            return f"document_similarity_{content_folder_name}_{algorithm}.csv"
+        raise ValueError("Document similarity or evaluation not specified.")
+
+    def _determine_last_index(self):
+        """Determine the last index processed in the CSV file if it exists."""
+        if os.path.exists(self.file_path):
+            with open(self.file_path, "r") as file:
+                reader = csv.reader(file)
+                return sum(1 for row in reader)
+        return -1
+
+    def _open(self):
+        """Open the CSV file for appending or writing, setting up the writer."""
+        self.csv_file = open(self.file_path, "a", newline="")
+        self.csv_writer = csv.writer(self.csv_file)
+        if self.last_index == -1:
+            self.write_header()
+
+    def write_header(self):
+        """Write the CSV header."""
+        self.csv_writer.writerow([
+            "page_id",
+            "dossier_id",
+            "retrieved_page_ids",
+            "retrieved_dossier_ids",
+            "scores",
+            "number_of_correct_dossiers",
+            *(f"dossier#{i+1}" for i in range(20)),
+        ])
+
+    def write_row(self, row_data):
+        """Write a row to the CSV file."""
+        self.csv_writer.writerow(row_data)
+
+    def close(self):
+        """Close the CSV file."""
+        if self.csv_file:
+            self.csv_file.close()
+
+    def get_last_index(self):
+        """Get the last processed index."""
+        return self.last_index
--- src/common/register_time.py
+++ src/common/register_time.py
@@ -1,18 +1,25 @@
+"""
+Time tracker class
+First line contains the start times of all sessions.
+Second line contains the estimated total time taken in seconds (float).
+"""
+
 import time
 import os
 from datetime import datetime
 
 
 class Timer:
-    def __init__(self, file_name, folder_name="time_logs"):
-        self.file_path = os.path.join(folder_name, file_name)
+    def __init__(self, content_folder_name, algorithm, evaluation_file="", document_similarity=False, folder_name="./evaluation/results"):
+        self.file_name = self._generate_file_name(content_folder_name, algorithm, evaluation_file, document_similarity)
+        self.file_path = os.path.join(folder_name, self.file_name)
         if not os.path.exists(folder_name):
             os.makedirs(folder_name)
         self.start_times = []
         self.elapsed_time = 0.0
-        self.load_initial_time()
+        self._load_initial_time()
 
-    def load_initial_time(self):
+    def _load_initial_time(self):
         if os.path.exists(self.file_path):
             with open(self.file_path, "r") as file:
                 lines = file.readlines()
@@ -29,8 +36,17 @@ class Timer:
         current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
         self.start_times.append(current_time)
         self.start_time = time.time()
+        print(f"[Info] ~ Starting time register with time: {current_time}", flush=True)
+        
+    def _generate_file_name(self, content_folder_name, algorithm, evaluation_file, document_similarity):
+        if len(evaluation_file) != 0:
+            evaluation_file = evaluation_file.split('.')[0]
+            return f"evaluation_{content_folder_name}_{evaluation_file}_{algorithm}_time.txt"
+        if document_similarity:
+            return f"document_similarity_{content_folder_name}_{algorithm}_time.txt"
+        raise ValueError("Document similarity or evaluation not specified.")
 
-    def save_time(self):
+    def update_time(self):
         if self.start_time is not None:
             elapsed_since_start = time.time() - self.start_time
             temporary_elapsed_time = self.elapsed_time + elapsed_since_start
@@ -43,3 +59,4 @@ class Timer:
                 file.write(f"{start_times_str}\n{temporary_elapsed_time}")
         else:
             raise ValueError("No start times to save.")
+        
