47d64c3b3bccaa03d406c66e8051333b008c39a7
NickyJu <Y.H.Ju@student.tudelft.nl>
2024-09-09 14:21:27 +0200

    preprocess stem stopwords

---
+++ src/preprocess_stem_stopwords.py
@@ -0,0 +1,37 @@
+"""
+This script reads in a dataset, and stems the words and removes dutch stopwords.
+And then saves it in the same format.
+
+Example with arguments:
+python preprocess_stem_stopwords.py
+"""
+
+import os
+import pandas as pd
+from argparse import ArgumentParser
+from common import evaluate_helpers
+
+def main():
+    parser = ArgumentParser()
+    parser.add_argument("--content_folder_name", type=str, required=True)
+    parser.add_argument("--documents_directory", type=str, required=True)
+    args = parser.parse_args()
+
+    # Selecting the paths
+    input_path = os.path.join(args.documents_directory, args.content_folder_name, "woo_merged.csv.gz")
+    woo_data = pd.read_csv(input_path, compression="gzip")
+
+    # Filter text
+    woo_data['bodyText'] = woo_data['bodyText'].apply(lambda x: evaluate_helpers.preprocess_text(str(x)))
+
+    new_file_name = args.content_folder_name + "_stem_stopwords"
+    save_directory = os.path.join(args.documents_directory, new_file_name)
+    if os.path.exists(save_directory):
+        raise FileExistsError(f"[Error] ~ The directory '{save_directory}' already exists.", flush=True)
+    os.makedirs(save_directory)
+    output_path = os.path.join(save_directory, "woo_merged.csv.gz")
+    woo_data.to_csv(output_path, compression="gzip", index=False)
+    print(f"[Info] ~ Processed data saved to {output_path}.")
+
+if __name__ == "__main__":
+    main()
