e74cc9f5518cb7d25660f524e2efbea247fe79c5
NickyJu <Y.H.Ju@student.tudelft.nl>
2024-09-30 10:47:55 +0200

    deprecate old file, rename create evaluation file

---
+++ src/OLD_create_evaluation_file.py
@@ -0,0 +1,83 @@
+# This script will generate an ground truth evaluation file for the requests in the woo-dossiers.
+# It will generate a JSON file with the following structure:
+# { "bodytext": {"pages": [page1, page2, ...], "documents": [document1, document2, ...], "dossier": [dossierId] }
+
+# Examples with arguments:
+# python create_evaluation_file.py --content_folder_name WoogleDumps_01-04-2024_12817_dossiers_12_dossiers --documents_directory ./docs --evaluation_directory ./evaluation
+# python create_evaluation_file.py --content_folder_name 12_dossiers --documents_directory ./docs --evaluation_directory ./evaluation
+
+import json
+import os
+import pandas as pd
+from argparse import ArgumentParser
+
+
+def main():
+    # Parse all the arguments and read the settings
+    parser = ArgumentParser()
+    parser.add_argument("--content_folder_name", type=str, required=True)
+    parser.add_argument("--documents_directory", type=str, required=True)
+    parser.add_argument("--evaluation_directory", type=str, required=True)
+    args = parser.parse_args()
+
+    content_folder_name = args.content_folder_name
+    documents_directory = args.documents_directory
+    evaluation_directory = args.evaluation_directory
+    print(f"[Info] ~ Source folder of documents: {content_folder_name}")
+
+    file_path = f"{documents_directory}/{content_folder_name}/woo_merged.csv.gz"
+    woo_data = pd.read_csv(file_path, compression="gzip")
+
+    # Find all requests
+    requests = woo_data[woo_data["type"].str.lower() == "verzoek"]
+    print("[Info] ~ Length requests: ", len(requests))
+
+    # Get dataframe without the requests
+    no_requests_dataframe = woo_data[woo_data["type"].str.lower() != "verzoek"]
+    print("[Info] ~ Length no requests df: ", len(no_requests_dataframe))
+
+    # Get the aggregated text for each dossier
+    # Structure: { foi_dossierId: bodytext_foi_bodyTextOCR }
+    aggregated_requests = requests.groupby("dossier_id")["bodyText"].apply(lambda texts: " ".join(map(str, texts))).to_dict()
+
+    # Create ground truth
+    # Structure: { foi_dossierId: { pages: [page1, page2, ...], documents: [document1, document2, ...] } }
+    aggregated_dict = (
+        no_requests_dataframe.groupby("dossier_id")
+        .apply(
+            lambda x: {
+                "pages": list(x["page_id"].unique()),
+                "documents": list(x["document_id"].unique()),
+            }
+        )
+        .to_dict()
+    )
+
+    # Merge bodytext and ground truth
+    # Structure: { bodytext: { pages: [page1, page2, ...], documents: [document1, document2, ...], dossier: [dossierId] } }
+    merged_structure = {}
+    for dossier_id, body_text in aggregated_requests.items():
+        normalized_body_text = " ".join(body_text.split())
+        if dossier_id in aggregated_dict:
+            merged_structure[normalized_body_text] = {
+                "pages": aggregated_dict[dossier_id]["pages"],
+                "documents": aggregated_dict[dossier_id]["documents"],
+                "dossier": [dossier_id],  # Encapsulating dossier_id in a list as per your requirement
+            }
+
+    json_file_path = os.path.join(
+        evaluation_directory,
+        f"evaluation_request_{content_folder_name}_no_requests.json",
+    )
+
+    # Check if the directory exists, if not, create it
+    if not os.path.exists(evaluation_directory):
+        os.makedirs(evaluation_directory)
+
+    # Write the aggregated JSON to a file
+    with open(json_file_path, "w") as file:
+        json.dump(merged_structure, file)
+
+
+if __name__ == "__main__":
+    main()
--- src/create_evaluation_file.py
+++ src/create_evaluation_file.py
@@ -1,10 +1,12 @@
-# This script will generate an ground truth evaluation file for the requests in the woo-dossiers.
-# It will generate a JSON file with the following structure:
-# { "bodytext": {"pages": [page1, page2, ...], "documents": [document1, document2, ...], "dossier": [dossierId] }
+"""
+This script will generate an ground truth evaluation file for the requests in the woo-dossiers.
+It will generate a JSON file with the following structure:
+{ "bodytext": {"pages": [page1, page2, ...], "documents": [document1, document2, ...], "dossier": [dossierId] }
 
-# Examples with arguments:
-# python create_evaluation_file.py --content_folder_name WoogleDumps_01-04-2024_12817_dossiers_12_dossiers --documents_directory ./docs --evaluation_directory ./evaluation
-# python create_evaluation_file.py --content_folder_name 12_dossiers --documents_directory ./docs --evaluation_directory ./evaluation
+Examples with arguments:
+python create_evaluation_file.py --content_folder_name minbzk --documents_directory ./final_docs_minbzk --evaluation_directory ./final_evaluation_minbzk
+python create_evaluation_file.py --content_folder_name minbzk --documents_directory ./final_docs_minbzk --evaluation_directory ./final_evaluation_minbzk --real_words
+"""
 
 import json
 import os
@@ -12,12 +14,19 @@ import pandas as pd
 from argparse import ArgumentParser
 
 
+def filter_body_text(text, words_set):
+    # Split text into words and filter them
+    filtered_words = [word for word in text.split() if word in words_set]
+    # Join the filtered words back into a string
+    return ' '.join(filtered_words)
+
 def main():
     # Parse all the arguments and read the settings
     parser = ArgumentParser()
     parser.add_argument("--content_folder_name", type=str, required=True)
     parser.add_argument("--documents_directory", type=str, required=True)
     parser.add_argument("--evaluation_directory", type=str, required=True)
+    parser.add_argument("--real_words", action="store_true")
     args = parser.parse_args()
 
     content_folder_name = args.content_folder_name
@@ -28,22 +37,35 @@ def main():
     file_path = f"{documents_directory}/{content_folder_name}/woo_merged.csv.gz"
     woo_data = pd.read_csv(file_path, compression="gzip")
 
+    # Check if each dossier_id group has at least one 'verzoek' or 'besluit'
+    has_verzoek = woo_data[woo_data["type"].str.lower() == "verzoek"].groupby("dossier_id").size() > 0
+    has_besluit = woo_data[woo_data["type"].str.lower() == "besluit"].groupby("dossier_id").size() > 0
+
+    # Check if each dossier_id group has at least one 'bijlage'
+    has_bijlage = woo_data[woo_data["type"].str.lower() == "bijlage"].groupby("dossier_id").size() > 0
+
+    # Combine the two masks to find dossier_ids that meet both conditions
+    valid_dossier_ids_verzoek = has_verzoek[has_verzoek].index.intersection(has_bijlage[has_bijlage].index)
+    valid_dossier_ids_besluit = has_besluit[has_besluit].index.intersection(has_bijlage[has_bijlage].index)
+
+    valid_dossier_ids = list(valid_dossier_ids_verzoek.union(valid_dossier_ids_besluit))
+
     # Find all requests
-    requests = woo_data[woo_data["type"].str.lower() == "verzoek"]
-    print("[Info] ~ Length requests: ", len(requests))
+    filtered_data = woo_data[woo_data["dossier_id"].isin(valid_dossier_ids)]
+    print("[Info] ~ Length filtered df: ", len(filtered_data))
 
     # Get dataframe without the requests
-    no_requests_dataframe = woo_data[woo_data["type"].str.lower() != "verzoek"]
-    print("[Info] ~ Length no requests df: ", len(no_requests_dataframe))
+    no_requests_filtered_dataframe = filtered_data[(filtered_data["type"].str.lower() != "verzoek") & (filtered_data["type"].str.lower() != "besluit")]
+    print("[Info] ~ Length no requests filtered df: ", len(no_requests_filtered_dataframe))
 
     # Get the aggregated text for each dossier
     # Structure: { foi_dossierId: bodytext_foi_bodyTextOCR }
-    aggregated_requests = requests.groupby("dossier_id")["bodyText"].apply(lambda texts: " ".join(map(str, texts))).to_dict()
+    aggregated_requests = filtered_data.groupby("dossier_id")["bodyText"].apply(lambda texts: " ".join(map(str, texts))).to_dict()
 
     # Create ground truth
     # Structure: { foi_dossierId: { pages: [page1, page2, ...], documents: [document1, document2, ...] } }
     aggregated_dict = (
-        no_requests_dataframe.groupby("dossier_id")
+        no_requests_filtered_dataframe.groupby("dossier_id")
         .apply(
             lambda x: {
                 "pages": list(x["page_id"].unique()),
@@ -53,21 +75,36 @@ def main():
         .to_dict()
     )
 
+    if args.real_words:
+        words_set = set()
+        with open("./common/stopwords/wordlist-ascii.txt", 'r') as file:
+            for line in file:
+                words_set.add(line.strip())
+
     # Merge bodytext and ground truth
     # Structure: { bodytext: { pages: [page1, page2, ...], documents: [document1, document2, ...], dossier: [dossierId] } }
     merged_structure = {}
     for dossier_id, body_text in aggregated_requests.items():
         normalized_body_text = " ".join(body_text.split())
+        if args.real_words:
+            normalized_body_text = filter_body_text(str(normalized_body_text), words_set)
         if dossier_id in aggregated_dict:
             merged_structure[normalized_body_text] = {
                 "pages": aggregated_dict[dossier_id]["pages"],
                 "documents": aggregated_dict[dossier_id]["documents"],
-                "dossier": [dossier_id],  # Encapsulating dossier_id in a list as per your requirement
+                "dossier": [dossier_id],
             }
+
+    # Counting all pages in the merged_structure
+    total_pages_count = sum(len(details["pages"]) for details in merged_structure.values())
+
+    print(f"[Info] ~ Data for {content_folder_name}:")
+    print(f"[Info] ~ Length of the data: {len(merged_structure)}")
+    print(f"[Info] ~ Length of #Pages: {total_pages_count}")
 
     json_file_path = os.path.join(
         evaluation_directory,
-        f"evaluation_request_{content_folder_name}_no_requests.json",
+        f"evaluation_request_{content_folder_name}.json",
     )
 
     # Check if the directory exists, if not, create it
